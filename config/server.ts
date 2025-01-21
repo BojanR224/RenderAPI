97aa3965b5861f0361e903205494bcde4361b3eb
BojanR224 <bojan.robev@gmail.com>
2025-01-21 12:27:02 +0100

    Initial Commit 5

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: ["T5+18McggTHKTF4oF/CbmQ==","j68uA4ucE1ZevQfMqbgQ1g==","0+o4oYVSDA/lbZ2QgO7nyQ==","1eTJ/NivuhsMuJY4dNEYlg=="],
+    keys: env.array('APP_KEYS'),
   },
 });
